075dc55cba1fb1a1f373832e521441a7a563815e
Christoffer Lind <christoffer.lind@yandex.com>
2026-04-02 12:01:43 +0200

    Added simple view with configurable style.json

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+// Interface for configuration messages sent to the webview
+interface MapConfig {
+	mapStyleUrl: string;
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,17 +17,101 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-maplibre-viewer" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-maplibre-viewer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-maplibre-viewer!');
-	});
+	// Register the Maps webview provider
+	const mapsViewProvider = new MapViewProvider(context.extensionUri);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider('mapsView', mapsViewProvider)
+	);
+
+	// Register configuration change listener
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeConfiguration((e) => {
+			if (e.affectsConfiguration('vscodeMaplibreViewer')) {
+				mapsViewProvider.updateConfiguration();
+			}
+		})
+	);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+class MapViewProvider implements vscode.WebviewViewProvider {
+	public static readonly viewType = 'mapsView';
+
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) {}
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+			localResourceRoots: [
+				vscode.Uri.joinPath(this._extensionUri, 'resources')
+			]
+		};
+
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+	}
+
+	/**
+	 * Updates the map configuration when settings change
+	 */
+	public updateConfiguration(): void {
+		if (this._view) {
+			const config = this.getConfiguration();
+			this._view.webview.postMessage({
+				type: 'configUpdate',
+				config: config
+			});
+		}
+	}
+
+	/**
+	 * Gets the current configuration from VS Code settings
+	 */
+	private getConfiguration(): MapConfig {
+		const config = vscode.workspace.getConfiguration('vscodeMaplibreViewer');
+		return {
+			mapStyleUrl: config.get<string>('mapStyleUrl') || 'https://demotiles.maplibre.org/style.json'
+		};
+	}
+
+	private _getHtmlForWebview(webview: vscode.Webview): string {
+		// Use a nonce to only allow specific scripts to be run
+		const nonce = getNonce();
+		
+		// Get the current configuration
+		const config = this.getConfiguration();
+
+		// Read the HTML template from the resources folder
+		const htmlPath = path.join(this._extensionUri.fsPath, 'resources', 'map-view.html');
+		let htmlContent = fs.readFileSync(htmlPath, 'utf8');
+
+		// Replace the placeholders with actual values
+		htmlContent = htmlContent.replace(/\$\{cspSource\}/g, webview.cspSource);
+		htmlContent = htmlContent.replace(/\$\{nonce\}/g, nonce);
+		htmlContent = htmlContent.replace(/\$\{mapStyleUrl\}/g, config.mapStyleUrl);
+
+		return htmlContent;
+	}
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
